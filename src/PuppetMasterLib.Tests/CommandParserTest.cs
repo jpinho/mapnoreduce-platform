432a800264f7a520c769712525fdb35aeea25bf3
João Pinho <jpe.pinho@gmail.com>
2015-03-18 17:33:36 +0000

    [ADD] PuppetMasterLib Unit Tests

---
+++ src/PuppetMasterLib.Tests/CommandParserTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PuppetMasterLib.Tests
+{
+    [TestClass]
+    public class CommandParserTest
+    {
+        [TestMethod]
+        public void TestParseCommands() {
+
+            string script = @"
+WORKER ...
+CENAS ...
+STATUS ...";
+
+
+        }
+    }
+}
