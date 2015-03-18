b29ab9649111b42c9c5d52f864a0eaf41b6f3954
Claudia Filipe <claudiapbfilipe@gmail.com>
2015-03-18 23:35:53 +0000

    [ADD] CommandParser.cs [ADD] CommandInvalidParameterException.cs

--- src/PuppetMasterLib.Tests/CommandParserTest.cs
+++ src/PuppetMasterLib.Tests/CommandParserTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuppetMasterLib;
+using System.Diagnostics;
+using System.Threading;
 
 namespace PuppetMasterLib.Tests
 {
@@ -9,13 +12,35 @@ namespace PuppetMasterLib.Tests
         [TestMethod]
         public void TestParseCommands() {
 
+            // arrange
+            bool createWorkerSuccess = false;
             string script = @"
-WORKER ...
-CENAS ...
-STATUS ...";
+WORKER 1 <PUPPETMASTER-URL> <SERVICE-URL> <ENTRY-URL>
+SUBMIT <ENTRY-URL> <FILE> <OUTPUT> 10 <MAP>
+%potato
+WAIT 5";
 
+            CommandParser cmdParser = new CommandParser() {
+                CreateWorker = delegate(int workerId, string puppetMasterUrl, string serviceUrl, string entryUrl){
+                    createWorkerSuccess = true;
+                    Debug.WriteLine("Hello, hello, hello!");
+                }
+            };
+            try
+            {
+                // act
+                cmdParser.execute(script);
+            }
+            catch (CommandInvalidParameterException){
+                Assert.IsTrue(true);
+                return;
+            }
+            //Assert.Fail("Invalid parameter type exception wasnt caught :(");
+            
+            Thread.Sleep(5000);
 
-
+            // assert 
+            Assert.IsTrue(createWorkerSuccess);
         }
     }
 }
