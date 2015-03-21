4192c70748828c44b91ac38c12ba0705ecde6127
João Pinho <jpe.pinho@gmail.com>
2015-03-21 04:11:25 +0000

    [ADD] Unit tests for ClientConnectorService

---
+++ src/ClientConnectorService.Tests/ClientSplitProviderServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClientConnectorService.Tests
+{
+    [TestClass]
+    public class ClientSplitProviderServiceTest
+    {
+        [TestMethod]
+        public void TestSplitAndSave() {
+            //TODO
+        }
+    }
+}
