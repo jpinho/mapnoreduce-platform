b61ef626a72e197abba5f7656c28968752d8d1a7
João Pinho <jpe.pinho@gmail.com>
2015-03-31 17:46:29 +0100

    [FIX] JobTask SplitNumber changed from string to int

--- src/PuppetMasterServer/JobTask.cs
+++ src/PuppetMasterServer/JobTask.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PlatformCore
+﻿namespace PlatformCore
 {
     public class JobTask : SharedTypes.IJobTask
     {
         public string FileName { get; set; }
-        public string SplitNumber { get; set; }
+        public int SplitNumber { get; set; }
         public string SplitProviderURL { get; set; }
         public string OutputReceiverURL { get; set; }
         public byte[] MapFunctionAssembly { get; set; }
