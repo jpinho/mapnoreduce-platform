86e389b3195beda78972b4d781786bbd907d7514
João Pinho <jpe.pinho@gmail.com>
2015-03-31 17:47:04 +0100

    [RFT] CodeMaid format

--- src/PuppetMasterServer/JobTracker.cs
+++ src/PuppetMasterServer/JobTracker.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SharedTypes;
 using System.Runtime.Remoting;
-using System.Runtime.Remoting.Channels;
-using System.Runtime.Remoting.Channels.Tcp;
-using System.Runtime.Serialization.Formatters;
-using System.Collections;
+using SharedTypes;
 
 namespace PlatformCore
 {
@@ -20,28 +14,22 @@ namespace PlatformCore
         private Dictionary<int, IWorker> activeWorkers = new Dictionary<int, IWorker>();
         private DateTime lastHeartBeat = DateTime.UtcNow.Date;
 
-        public JobTracker(Worker worker)
-        {
+        public JobTracker(Worker worker) {
             this.worker = worker;
         }
 
-        public void Start(JobTrackerStatus trackerMode)
-        {
-            // Register Services (alive, complete)
-            // Do Work
-            if (Enum.Equals(trackerMode, JobTrackerStatus.ACTIVE))
-            {
+        public void Start(JobTrackerStatus trackerMode) {
+            // Register Services (alive, complete) Do Work
+            if (Enum.Equals(trackerMode, JobTrackerStatus.ACTIVE)) {
                 RemotingServices.Marshal(this, "JobTracker", typeof(IJobTracker));
             }
         }
 
-        public void Alive(int wid)
-        {
+        public void Alive(int wid) {
             /*TODO fault tolerant algorithm*/
         }
 
-        public void Complete(int wid)
-        {
+        public void Complete(int wid) {
             //Remove worker from active list
             activeWorkers.Remove(wid);
         }
