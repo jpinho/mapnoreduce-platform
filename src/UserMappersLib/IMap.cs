92d0c715b897c8d58c1e4eb56ceaac134aa1ed54
João Pinho <jpe.pinho@gmail.com>
2015-04-08 15:14:47 +0100

    [UPD] Removed referenced of SharedTypes from UsersMappersLib
    This will avoid a problem that would happen later, related with DLL dependency.
    Creating a dependency between UserMappersLib.dll and SharedTypes.dll would force us to send the byte code of the SharedTypes.dll also.
    Therefore I copied the IMap interface to the UserMappersLib, to avoid DLL dependencies.

---
+++ src/UserMappersLib/IMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserMappersLib
+{
+    public interface IMap
+    {
+        IList<KeyValuePair<String, String>> Map(string fileLine);
+    }
+}
--- src/UserMappersLib/MonkeyMapper.cs
+++ src/UserMappersLib/MonkeyMapper.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace UserMappersLib
 {
-    public class MonkeyMapper : SharedTypes.IMap
+    public class MonkeyMapper : IMap
     {
         #region IMap Members
 
