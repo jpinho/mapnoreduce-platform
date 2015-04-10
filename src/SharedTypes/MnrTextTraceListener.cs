e23c0b428e5e2c9008c915e9a86ae8a97708b7cc
João Pinho <jpe.pinho@gmail.com>
2015-04-10 04:17:32 +0100

    [ADD] MapNoReduce Text Trace Listener configured in all projects App.config
    This log enables you to run and see all Traces on a file local to the runned project.
    The log file was name 'mnr-trace.log'.
    This file will be used to display the operation verbose progress of the runned scripts.

---
+++ src/SharedTypes/MnrTextTraceListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SharedTypes
+{
+    public class MnrTextTraceListener : System.Diagnostics.TextWriterTraceListener
+    {
+        private const string MASK = "MAPNOREDUCE :: {0} :: ";
+        private const string TFORMAT = "dd-MM-yyTHH:mm:ss";
+
+        public MnrTextTraceListener(string file)
+            : base(file) {
+        }
+
+        public override void WriteLine(string message) {
+            this.Write(string.Format(MASK, DateTime.Now.ToString(TFORMAT)));
+            base.WriteLine(message);
+        }
+    }
+}
