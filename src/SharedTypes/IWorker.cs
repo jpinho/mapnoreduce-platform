1a71bec8a140e01c8ca49b120a67371d097393b5
João Pinho <jpe.pinho@gmail.com>
2015-04-10 14:16:05 +0100

    [FIX] Worker GetStatus now returns the actual status of the worker besides writing to Trace file (and not Debug)

--- src/SharedTypes/IWorker.cs
+++ src/SharedTypes/IWorker.cs
@@ -9,7 +9,7 @@ namespace SharedTypes
 
         bool ExecuteMapJob(IJobTask task);
         void ReceiveMapJob(IJobTask job);
-        void GetStatus();
+        WorkerStatus GetStatus();
         void Slow(int secs);
         void Freeze();
         void UnFreeze();
