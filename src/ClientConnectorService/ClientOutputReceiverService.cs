94205f12d5816477b1b0902da2d823afe93e7e77
João Pinho <jpe.pinho@gmail.com>
2015-04-10 04:21:06 +0100

    [ADD] Implemented the ReceiveMapOutputFragment and the IsMapResultReady
    This enables the ClientService to finish its execution.
    The IsMapResultReady was returning false, thus leading ClientService to stay on an infinity loop.

--- src/ClientConnectorService/ClientOutputReceiverService.cs
+++ src/ClientConnectorService/ClientOutputReceiverService.cs
@@ -1,38 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SharedTypes;
 
 namespace ClientServices
 {
     internal class ClientOutputReceiverService : MarshalByRefObject, IClientOutputReceiverService
     {
+        private readonly Dictionary<string, List<KeyValuePair<int, string[]>>> mapResultStore
+            = new Dictionary<string, List<KeyValuePair<int, string[]>>>();
+
         public string ServiceURL { get; private set; }
 
         public ClientOutputReceiverService(string serviceURL) {
             ServiceURL = serviceURL;
         }
 
-        private Dictionary<string, List<KeyValuePair<int, string>>> mapResultStore = new Dictionary<string, List<KeyValuePair<int, string>>>();
+        public void ReceiveMapOutputFragment(string filePath, string[] result, int splitNumber) {
+            lock (mapResultStore) {
+                if (!mapResultStore.ContainsKey(filePath))
+                    mapResultStore.Add(filePath, new List<KeyValuePair<int, string[]>>());
 
-        public void ReceiveMapOutputFragment(string filePath, string result, int splitNumber) {
-            if (!mapResultStore.ContainsKey(filePath))
-                mapResultStore.Add(filePath, new List<KeyValuePair<int, string>>());
-
-            mapResultStore[filePath].Add(new KeyValuePair<int, string>(splitNumber, result));
-            //TODO: trigger event to notify the receival of a new result from a file and to alert that the worker finished that split.
+                mapResultStore[filePath].Add(new KeyValuePair<int, string[]>(splitNumber, result));
+                //TODO: trigger event to notify the receival of a new result from a file and to alert that the worker finished that split.
+            }
         }
 
-        internal string[] GetMapResult() {
-            //TODO
-            return null;
+        public List<string[]> GetMapResult(string filePath) {
+            lock (mapResultStore) {
+                if (!mapResultStore.ContainsKey(filePath))
+                    return null;
+
+                return (
+                    from r in mapResultStore[filePath]
+                    orderby r.Key ascending
+                    select r.Value
+                ).ToList();
+            }
         }
 
-        internal bool IsMapResultReady(string filePath, int nSplits) {
-            //TODO
-            return false;
+        public bool IsMapResultReady(string filePath, int nSplits) {
+            lock (mapResultStore) {
+                var notContainsKey = !mapResultStore.ContainsKey(filePath);
+                var isNotYetComplete = mapResultStore.ContainsKey(filePath) && mapResultStore[filePath].Count < nSplits;
+
+                // if both 'false' the result is ready.
+                return (!notContainsKey && !isNotYetComplete);
+            }
         }
     }
 }
