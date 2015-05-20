229b5d6a477e217e30e27bb9bc68263a3f0e8d58
João Pinho <jpe.pinho@gmail.com>
2015-05-20 14:43:00 +0100

    [ADD] Time Mapper

---
+++ src/SampleMappers/LibMapperTime/IMapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LibMapperTime
+{
+    public interface IMapper
+    {
+        IList<KeyValuePair<string, string>> Map(string fileLine);
+    }
+}
---
+++ src/SampleMappers/LibMapperTime/TimeMapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace LibMapperTime
+{
+    public class TimeMapper : IMapper
+    {
+        private static uint lineNo = 0;
+        public IList<KeyValuePair<string, string>> Map(string fileLine) {
+            lineNo++;
+            Thread.Sleep(1000);
+            IList<KeyValuePair<string, string>> result = new List<KeyValuePair<string, string>>();
+            result.Add(new KeyValuePair<string, string>(lineNo.ToString(), fileLine));
+            return result;
+        }
+    }
+}
